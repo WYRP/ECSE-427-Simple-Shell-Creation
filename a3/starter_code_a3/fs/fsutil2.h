5b7f9c3ac08d1f482dab568cd3009371551f34b4
Y. Pan <yongru.pan@mail.mcgill.ca>
2024-03-12 21:25:37 -0400

    Fixed some minor mistake on the fsutil2.h file

--- a3/starter_code_a3/fs/fsutil2.h
+++ a3/starter_code_a3/fs/fsutil2.h
@@ -1,6 +1,8 @@
 #ifndef FILESYS_FSUTIL2_H
 #define FILESYS_FSUTIL2_H
 
+int get_file_size(char *fname);
+int get_free_space();
 int copy_in(char *fname);
 int copy_out(char *fname);
 void find_file(char *pattern);
