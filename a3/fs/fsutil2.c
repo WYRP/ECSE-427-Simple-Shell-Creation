e604ebf32765e7db70866f69dfdbfd3800cf1f01
Yixuan Qin <yixuan.qin@mail.mcgill.ca>
2024-03-31 19:29:08 -0400

    Update fsutil2.c

--- a3/fs/fsutil2.c
+++ a3/fs/fsutil2.c
@@ -181,11 +181,10 @@ int defragment() {
     struct inode_disk *disk_inode = &fileNode->data;
 
     free_map_release(inode_sector, bytes_to_sectors(fileSize) + 1);
-    fileNode->sector = inode_sector;
-    
-    buffer_cache_write(inode_sector, disk_inode);
-    free_map_allocate(bytes_to_sectors(fileSize), &inode_sector);
-
+    if(free_map_allocate(bytes_to_sectors(fileSize), &inode_sector)){
+      buffer_cache_write(inode_sector, disk_inode);
+      fileNode->sector = inode_sector;
+    }
     inode_write_at(fileNode, buffer, fileSize, offset);
     offset += fileSize;
     sector_offset += bytes_to_sectors(fileSize);
