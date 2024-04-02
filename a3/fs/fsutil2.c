d82c6db48ebc20e9685fa846602c97e6e8d0ca4a
Yixuan Qin <yixuan.qin@mail.mcgill.ca>
2024-04-02 15:15:22 -0400

    Update fsutil2.c

--- a3/fs/fsutil2.c
+++ a3/fs/fsutil2.c
@@ -309,7 +309,7 @@ void recover(int flag) {
       //remove the last part of the file
       // remove_first_n_chars(buffer, num_overflow_chars);
       //remove null bytes
-      remove_nulls(buffer, strlen(buffer));
+      remove_nulls(buffer, BLOCK_SECTOR_SIZE);
 
       char fname[100];
       sprintf(fname, "recovered2-%s.txt", name);
